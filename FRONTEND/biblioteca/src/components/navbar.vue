3f9946db057ca473d6b0cdd974505924a57c8b82
Hamilton <hamiltonmb.silva@gmail.com>
2020-03-07 23:35:59 -0300

    Criação da tela de login

---
+++ FRONTEND/biblioteca/src/components/navbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="navbar">
+    <b-navbar type="dark" variant="dark">
+      <b-navbar-nav>
+        <b-navbar-brand>
+          <router-link class="link" to="/login">Login</router-link>
+        </b-navbar-brand>
+        <b-nav-item>
+          <!-- <router-link class="link" to="/paciente">Paciente</router-link> -->
+        </b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped>
+.link {
+  color: white;
+  text-decoration: none;
+  font-size: 18px;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.link:hover {
+  color: gray;
+}
+</style>
--- FRONTEND/biblioteca/src/main.js
+++ FRONTEND/biblioteca/src/main.js
@@ -1,8 +1,28 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import BootstrapVue from "bootstrap-vue";
+import VueRouter from "vue-router";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(VueRouter);
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false
 
+import Login from "./pages/login.vue";
+
+const routes = [
+  {path: "/login", component: Login}
+];
+
+const router = new VueRouter({
+  routes,
+  mode: "history" //remove o # da url
+});
+
 new Vue({
+  router,
+  el:"#app",
   render: h => h(App),
 }).$mount('#app')
